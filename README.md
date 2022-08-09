423f01778d9f452a09f3fd262168b4cda79c9e90
kanav12345 <107997084+kanav12345@users.noreply.github.com>
2022-08-09 20:44:53 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Project-23
+# C21SA
---
+++ sketch.js
@@ -0,0 +1,67 @@
+
+
+
+//Create namespace for Engine
+const Engine=Matter.Engine 
+//Create namespace for World
+const World=Matter.World
+//Create namespace for Bodies
+const Bodies=Matter.Bodies
+//Create namespace for Body
+
+var ground,ball
+var engine, world
+
+
+function setup() {
+  createCanvas(400,400);
+//create the engine
+engine=Engine.create()
+  //Add world to the engine
+  world = engine.world
+
+  
+   var ball_options = {
+    restitution: 0.95,
+    frictionAir:0.01
+  }
+   
+   var ground_options ={
+     isStatic: true
+   };
+  
+  
+//create a ground
+ground = Bodies.rectangle(200,370,400,15,ground_options)
+//add to world
+World.add(world,ground)
+
+  ball = Bodies.circle(200,10,20,ball_options);
+  World.add(world,ball);
+  
+  
+  
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+}
+
+
+function draw() 
+{
+  background(51);
+  Engine.update(engine);
+  
+  fill("orange")
+
+  ellipse(ball.position.x,ball.position.y,20);
+  fill("green")
+  rect(ground.position.x,ground.position.y,400,15)
+  //write a rectangle function to display ground.
+ 
+
+
+  
+  
+}
+
